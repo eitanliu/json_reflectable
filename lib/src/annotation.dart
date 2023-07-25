ef5a6040fbd4de88d42fc4d1a2fc4776d03a3381
Eitan Liu <eitanliu@gmail.com>
2023-07-26 00:49:30 +0800

    add formJsonOrNull/toJsonOrNull

--- lib/src/annotation.dart
+++ lib/src/annotation.dart
@@ -2,6 +2,9 @@ import 'package:reflectable/reflectable.dart';
 
 /// Annotate with this class to enable reflection.
 class JsonReflector extends Reflectable {
+  static const _defFromJsonName = 'fromJson';
+  static const _defToJsonName = 'toJson';
+
   const JsonReflector()
       : super(
           // Request the capability to invoke methods.
@@ -14,7 +17,7 @@ class JsonReflector extends Reflectable {
   /// [type] instance class type
   Iterable<T> formIterable<T>(
     Iterable iterable, {
-    String name = "fromJson",
+    String name = _defFromJsonName,
     Type? type,
   }) {
     return iterable.map((e) {
@@ -28,7 +31,7 @@ class JsonReflector extends Reflectable {
   /// [type] instance class type
   Iterable<T?> formIterableNullable<T>(
     Iterable iterable, {
-    String name = "fromJson",
+    String name = _defFromJsonName,
     Type? type,
   }) {
     return iterable.map((e) {
@@ -37,13 +40,26 @@ class JsonReflector extends Reflectable {
     });
   }
 
+  /// parse iterable
+  /// [iterable] iterable map
+  /// [name] constructor name
+  /// [type] instance class type
+  T? formJsonOrNull<T>(
+    Map<String, dynamic>? json, {
+    String name = _defFromJsonName,
+    Type? type,
+  }) {
+    if (json == null) return null;
+    return formJson<T>(json, name: name);
+  }
+
   /// parse json
   /// [json] json map
   /// [name] constructor name
   /// [type] instance class type
   T formJson<T>(
     Map<String, dynamic> json, {
-    String name = "fromJson",
+    String name = _defFromJsonName,
     Type? type,
   }) {
     final classMirror = reflectType(type ?? T) as ClassMirror;
@@ -54,9 +70,20 @@ class JsonReflector extends Reflectable {
   /// format json
   /// [instance] json instance class
   /// [name] function name
+  Map<String, dynamic>? toJsonOrNull<T extends Object?>(
+    T instance, {
+    String name = _defToJsonName,
+  }) {
+    if (instance == null) return null;
+    return toJson(instance, name: name);
+  }
+
+  /// format json
+  /// [instance] json instance class
+  /// [name] function name
   Map<String, dynamic> toJson<T extends Object>(
     T instance, {
-    String name = "toJson",
+    String name = _defToJsonName,
   }) {
     InstanceMirror instanceMirror = reflect(instance);
     return instanceMirror.invoke(name, []) as Map<String, dynamic>;
